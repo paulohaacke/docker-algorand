dd9a9c529d87a057b5b9d22a999c461e8b87c00a
Paulo Haacke <paulohaacke@gmail.com>
2021-06-19 00:32:00 -0300

    Including parsing for EndpointAddress field inside network.json

--- 2.6.0/buster-slim/entrypoint.sh
+++ 2.6.0/buster-slim/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+NODE_ENDPOINT_STRINGS=$(jq '.Nodes[] | select(.EndpointAddress!=null) | .Name,.EndpointAddress' /etc/algorand/network.json | tr '\r\n' ' ')
+IFS=', ' read -r -a NODE_ENDPOINT_ARRAY <<< "$NODE_ENDPOINT_STRINGS"
+
+for i in "${!NODE_ENDPOINT_ARRAY[@]}"; do 
+  if [ $((i%2)) -eq 0 ]; then
+    NODE_CONFIG_FILE=$(eval echo "~/net1/${NODE_ENDPOINT_ARRAY[$i]}/config.json")
+  else
+    ENDPOINT=$(eval echo ${NODE_ENDPOINT_ARRAY[$i]})
+    echo "$(cat $NODE_CONFIG_FILE | jq --arg ENDPOINT $ENDPOINT '. + {EndpointAddress: $ENDPOINT}')" > ${NODE_CONFIG_FILE}
+  fi
+done
+
 goal network create -r ~/net1 -n private -t /etc/algorand/network.json
 goal network start -r ~/net1
 
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Algorand network private docker image.
 ```shell
 docker run -it --rm -v "$(pwd)/network-template.json":/etc/algorand/network.json" paulohaacke/algorand-private-network:2-buster-slim
 ```
+
+# Issues
+
+A field called "EndpointAddress" could be added to a Node inside the network.json template in order to change the IP address algod is listening on.
