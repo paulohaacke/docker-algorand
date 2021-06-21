7780ccd4b0ac9038d969c12ad7833760ffc9afdc
Paulo Haacke <paulohaacke@gmail.com>
2021-06-20 23:01:29 -0300

    Including KMD and Indexer and changing some other things

--- 2.6.0/buster-slim/entrypoint.sh
+++ 2.6.0/buster-slim/entrypoint.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 
+# Initialize Database
+pg_ctlcluster ${POSTGRES_VERSION} main start \
+  && runuser -u postgres -- psql --command "CREATE USER algorand_indexer WITH SUPERUSER PASSWORD 'indexer';"
+
 # Create network
 goal network create -r ~/net1 -n private -t /etc/algorand/network.json
 
 # Change config.json to include "EndpointAddress"
-NODE_ENDPOINT_STRINGS=$(jq '.Nodes[] | select(.EndpointAddress!=null) | .Name,.EndpointAddress' /etc/algorand/network.json | tr '\r\n' ' ')
-IFS=', ' read -r -a NODE_ENDPOINT_ARRAY <<< "$NODE_ENDPOINT_STRINGS"
-
-for i in "${!NODE_ENDPOINT_ARRAY[@]}"; do 
-  if [ $((i%2)) -eq 0 ]; then
-    NODE_CONFIG_FILE=$(eval echo "~/net1/${NODE_ENDPOINT_ARRAY[$i]}/config.json")
-  else
-    ENDPOINT=$(eval echo ${NODE_ENDPOINT_ARRAY[$i]})
-    echo "$(cat $NODE_CONFIG_FILE | jq --arg ENDPOINT $ENDPOINT '. + {EndpointAddress: $ENDPOINT}')" > ${NODE_CONFIG_FILE}
-  fi
+port_index=8080
+for config_file in ~/net1/**/config.json; do
+  #sed -i "s/127\.0\.0\.1\:0/0\.0\.0\.0:${port_index}/g" ${config_file} && ((port_index++))
+  echo "$(cat $config_file | jq --arg ENDPOINT "0.0.0.0:${port_index}" '. + {EndpointAddress: $ENDPOINT}')" > ${config_file}
+  ((port_index++))
 done
 
 # Start Network
 goal network start -r ~/net1
 
+# Start Indexer
+INDEXER_NODES_STRINGS="${ALGORAND_INDEXER_NODES:-Participant}"
+IFS=', ' read -r -a INDEXER_NODES_ARRAY <<< "$INDEXER_NODES_STRINGS"
+for i in "${!INDEXER_NODES_ARRAY[@]}"; do 
+  node=${INDEXER_NODES_ARRAY[$i]}
+  node_dir=~/net1/${node}/
+  db_name="$(echo $node | tr '[:upper:]' '[:lower:]')_ledgerdb"
+  (runuser -u postgres -- createdb -O algorand_indexer ${db_name}) \
+  && algorand-indexer daemon -S ":$((i+8980))" -P "host=localhost port=5432 user=algorand_indexer password=indexer dbname=${db_name} sslmode=disable" -d ${node_dir} &
+done
+
+# Start and Expose KMD
+KMD_NODES_STRINGS="${ALGORAND_KMD_NODES:-Participant}"
+IFS=', ' read -r -a KMD_NODES_ARRAY <<< "$KMD_NODES_STRINGS"
+for i in "${!KMD_NODES_ARRAY[@]}"; do 
+  node=${KMD_NODES_ARRAY[$i]}
+  node_dir=~/net1/${node}
+  echo "{\"address\": \"0.0.0.0:$((i+9080))\"}" > ${node_dir}/kmd-v0.5/kmd_config.json
+  goal kmd start -d ${node_dir}
+done
+
 # Get stdout and stderr output files
-ALGOD_PIDS=$(pidof algod)
-TAIL_FILES=""
+ALGOD_PIDS="$(pidof algod algorand-indexer)"
+TAIL_FILES="/var/log/postgresql/*.log"
 for pid in ${ALGOD_PIDS}; do
   TAIL_FILES="${TAIL_FILES} /proc/${pid}/fd/1 /proc/${pid}/fd/2"
 done
--- README.md
+++ README.md
@@ -5,9 +5,24 @@ Algorand network private docker image.
 # How to use this image
 
 ```shell
-docker run -it --rm -v "$(pwd)/network-template.json":/etc/algorand/network.json" paulohaacke/algorand-private-network:2-buster-slim
+docker run -it -p8080:8080 --rm -v "$(pwd)/network-template.json":/etc/algorand/network.json" paulohaacke/algorand-private-network:2-buster-slim
 ```
 
-# Issues
+# Environment Variables
 
-A field called "EndpointAddress" could be added to a Node inside the network.json template in order to change the IP address algod is listening on.
+## ALGORAND_INDEXER_NODES:
+
+String containing node names separated by a space. Each node identified inside this variable will have an algorand-indexer tracking it and listening on port 8980 or greater.
+
+## ALGORAND_KMD_NODES:
+
+String containing node names separated by a space. For each node inside this string will have a KMD service exposed at port 9080 or greater.
+
+# Ports
+
+* Algod REST API: 8080 or greater
+* Indexer REST API: 8980 or greater
+* KMD Rest API: 9080 or greater
+
+Some ports are incremented when there are more than one node configured.
+
